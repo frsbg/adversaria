cc9d367c5da65f2a68523fd1157364b9383cfc6e
Feras Baig <ferasbaig@gmail.com>
2021-06-25 21:03:06 -0500

    update

---
+++ src/adversaria/settings.py
@@ -0,0 +1,12 @@
+class SchemeMetadata:
+    # formal settings for state management and metadata used to track users with their unique video copies
+    pass
+
+class AdversarialAttackConfig:
+    # specify config data of adversarial attack
+    pass
+
+
+class AdversariaMetadata:
+    # aggregate both scheme and attack config data here
+
